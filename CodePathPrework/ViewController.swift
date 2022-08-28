4ad078b1315729244608778b7861ad0e5ca1fe3a
Loc Tran <loctranforbusiness@gmail.com>
2022-08-28 17:01:44 -0400

    Change app functions and edit README

--- CodePathPrework/ViewController.swift
+++ CodePathPrework/ViewController.swift
@@ -11,18 +11,35 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
     
-
+    
+    @IBOutlet weak var NameField: UITextField!
     @IBOutlet weak var TextLabel: UILabel!
+    
     @IBAction func ButtonClicked(_ sender: Any) {
         print("Hello World, I am learning IOS Development")
         TextLabel.textColor = UIColor.systemMint
     }
+    
     @IBAction func Button2Clicked(_ sender: Any) {
         view.backgroundColor = UIColor.lightGray
         TextLabel.text = "Goodbye :( "
+        TextLabel.textColor = UIColor.darkGray
+    }
+    
+    @IBAction func GetName(_ sender: UITextField) {
+        var name = NameField.text!
+        if name.isEmpty {
+            name = "World"
+        }
+
+        let greeting = "Hello, \(name)!"
+        TextLabel.text = greeting
+        NameField.text = ""
+        TextLabel.textColor = UIColor.systemMint
+        view.backgroundColor = UIColor.systemMint
     }
 }
 
